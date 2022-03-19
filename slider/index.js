5800cf9ecee01b1b7bdd633284e0fb3b33d88454
Harman <harman.kaur@yara.com>
2022-03-19 17:41:20 +0100

    feat!: sliding animation on mouse and touch events

--- slider/index.js
+++ slider/index.js
@@ -1,3 +1,80 @@
 import './index.scss';
 
-console.log('working');
+class Slider {
+  parent = null;
+  slider = null;
+  startX = null;
+  index = 0;
+
+  constructor(parent, slider) {
+    this.parent = parent;
+    this.slider = slider;
+
+    // initialize
+    this.#init();
+  }
+
+  get elem() {
+    return this.slider;
+  }
+
+  #init() {
+    Array.from(this.slider.children).forEach(
+      (slide, i) => (slide.style.transform = `translateX(${i * 100}%)`),
+    );
+    this.parent.addEventListener('mousedown', e => {
+      this.onMouseDown(e);
+    });
+    this.parent.addEventListener('touchstart', e => {
+      this.onTouchStart(e);
+    });
+    this.parent.addEventListener('mouseup', e => {
+      this.onMouseUp(e);
+    });
+    this.parent.addEventListener('touchend', e => {
+      this.onTouchEnd(e);
+    });
+  }
+
+  #movePage(x) {
+    if (x - this.startX !== 0) {
+      if (x - this.startX > 0) {
+        this.index = Math.max(0, this.index - 1);
+        this.slider.style.transform = `translateX(-${100 * this.index}%)`;
+      } else {
+        this.index = Math.min(this.index + 1, this.slider.children.length - 1);
+        this.slider.style.transform = `translateX(-${100 * this.index}%)`;
+      }
+    }
+  }
+
+  // sliding event start
+  onMouseDown(e) {
+    this.startX = e.pageX;
+  }
+
+  onTouchStart(e) {
+    this.startX = e.changedTouches?.[0]?.clientX;
+  }
+
+  // sliding event ends
+  onMouseUp(e) {
+    this.#movePage(e.pageX);
+    // reset values
+    this.startX = null;
+  }
+
+  onTouchEnd(e) {
+    this.#movePage(e.changedTouches?.[0]?.clientX);
+    // reset values
+    this.startX = null;
+  }
+}
+
+const parentElem = '.slider';
+const sliderElem = '.slider__wrapper';
+
+new Slider(
+  document.querySelector(parentElem),
+  document.querySelector(sliderElem),
+);
