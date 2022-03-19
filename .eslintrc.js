957d2cdcad9c7b9845705efe0bdb51876632b1fe
Harman <harman.kaur@yara.com>
2022-03-19 22:40:28 +0100

    ci: eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,15 @@ module.exports = {
   parser: '@babel/eslint-parser',
   extends: ['airbnb-base', 'prettier'],
   plugins: ['import'],
+  env: {
+    browser: true,
+  },
+  rules: {
+    'no-param-reassign': [2, { props: false }],
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+  },
 };
