192110eb31029a67445046aab36473efd46b0dfd
Harman <harman.kaur@yara.com>
2022-03-20 09:48:54 +0100

    feat: logic to handle click of tiles
    1. get the x and y coordinates of the tile clicked
    2. swap the empty and the clicked tile
    3. remove previous click handlers
    4. add new click handlers to tiles around the new empty tile

--- puzzle/index.js
+++ puzzle/index.js
@@ -14,6 +14,12 @@ class Puzzle {
   constructor(goalState, tiles) {
     this.tiles = tiles;
     this.goalState = goalState;
+
+    this.eventListener = function eventListener(e) {
+      this.#onClickHandler(e.currentTarget);
+    };
+
+    this.clickHandler = this.eventListener.bind(this);
   }
 
   init() {
@@ -70,14 +76,59 @@ class Puzzle {
     return draggableElements;
   }
 
+  #getXYIndicesOfSelectedTile(tileText) {
+    const gridSize = Math.sqrt(this.puzzle.length);
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        if (this.puzzleGrid[i][j] === tileText) {
+          return { row: i, col: j };
+        }
+      }
+    }
+
+    return { row: null, col: null };
+  }
+
+  #onClickHandler(tile) {
+    this.#removeClickHandlers();
+
+    const { row, col } = this.#getXYIndicesOfSelectedTile(tile.innerText);
+
+    if (row !== null && col !== null) {
+      // swap empty and tile clicked in puzzle grid
+      this.puzzleGrid[this.emptyRowIndex][this.emptyColumnIndex] =
+        tile.innerText;
+      this.puzzleGrid[row][col] = '0';
+
+      // move tile
+      tile.style['grid-area'] = `${this.emptyRowIndex + 1} / ${
+        this.emptyColumnIndex + 1
+      }`;
+      this.emptyRowIndex = row;
+      this.emptyColumnIndex = col;
+
+      this.#setClickHandlers();
+    }
+  }
+
   #setClickHandlers() {
     const elements = this.#getAdjacentElements();
+    const self = this;
 
     this.tiles.forEach(tile => {
       if (elements.includes(tile.innerText)) {
-        tile.addEventListener('click', () => {
-          // handleClick here
-        });
+        tile.addEventListener('click', self.clickHandler);
+      }
+    });
+  }
+
+  #removeClickHandlers() {
+    const elements = this.#getAdjacentElements();
+    const self = this;
+
+    this.tiles.forEach(tile => {
+      if (elements.includes(tile.innerText)) {
+        tile.removeEventListener('click', self.clickHandler);
       }
     });
   }
